11c171da874ad00e1ae6060746709694290e58f3
KOPPUNOOR BHANU PRAKASH REDDY <koppunoorreddy@jklu.edu.in>
2023-03-01 15:11:45 +0530

    created login form

---
+++ home-services/src/components/loginform.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./loginform.css"
+
+
+const LoginForm = () => {
+    return ( <
+        div >
+
+        <
+        /div>
+    )
+}
+
+
+export default LoginForm
