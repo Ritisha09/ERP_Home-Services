680b8a9889e5d173b3b15d6e74b9c5086f7fe569
Ritisha09 <ritishamathur149@gmail.com>
2023-04-05 16:09:34 +0530

    added customer model and routes

---
+++ back-end/routes/customer.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const Customer = require('../models/customer');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// route to add in front end to call this API
+app.post('/add-customer' , async(req,res) => {
+    // getting variables from frontend
+    const {id,name,phone,address} = req.body;
+
+    let customer = await Customer.findOne({name,phone});
+    if(customer){
+        return res.status(400).json({error: "Customer already exists."});
+    }
+
+    customer = Customer({id,name,phone,address});
+
+    try{
+        await customer.save();
+        res.status(201).json({message: "Customer added successfully!!"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+module.exports = app;
--- back-end/server.js
+++ back-end/server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/connectDB');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/connectDB");
 
-const userRoutes = require('././routes/user')
-const empRoutes = require('././routes/employee')
+const userRoutes = require("././routes/user");
+const empRoutes = require("././routes/employee");
+const custRoutes = require("././routes/customer");
 
 const app = express();
 
 app.use(express.json());
 dotenv.config();
 
-PORT = process.env.PORT
+PORT = process.env.PORT;
 
 connectDB();
 
-app.get('/',(req,res) => {
-    res.send("API is running...")
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.use('/', userRoutes);
-app.use('/', empRoutes);
+app.use("/", userRoutes);
+app.use("/", empRoutes);
+app.use("/", custRoutes);
 
 app.listen(PORT, console.log(`Server started at ${PORT}`));
