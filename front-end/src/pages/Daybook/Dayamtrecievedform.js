0c171822d1e5cbd6dab5db49237862fe979ff708
Ritisha09 <76040190+Ritisha09@users.noreply.github.com>
2023-04-11 12:41:16 +0530

    Update Dayamtrecievedform.js

--- front-end/src/pages/Daybook/Dayamtrecievedform.js
+++ front-end/src/pages/Daybook/Dayamtrecievedform.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import SidebarLeft from "../../components/SidebarLeft"
 import "./Dayform.css"
 
@@ -13,7 +13,7 @@ function Dayamtrecievedform() {
     const id = "1";
     const date = document.getElementById('date').value;
     const amtrec = document.getElementById('amtrec').value;
-    const moneyrecfrom = document.getElementById('moneyrf').value;
+    const moneyrecfrom = document.getElementById('moneyrecfrom').value;
     const reason = document.getElementById('reason').value;
     const foliono = document.getElementById('folionum').value;
     try {
@@ -22,7 +22,7 @@ function Dayamtrecievedform() {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({id,date,amtrec,moneyrecfrom,reason,foliono})
+        body: JSON.stringify({id, date, amtrec, moneyrecfrom, reason, foliono})
       });
 
       const data = await response.json();
@@ -31,15 +31,15 @@ function Dayamtrecievedform() {
       if(response.status === 400 && data.error === "Item already exists."){
         // setIsRegistered(true);
         setError(data.error);
+      }else{
+        setError("");
       }
       // edge case
       e.target.reset();
-      setError(data.error);
     } catch (error) {
       console.log('error');
     }
   };
-
 
 
   return (
@@ -62,12 +62,12 @@ function Dayamtrecievedform() {
         </header>
 
           <form className="neumorphic-form" onSubmit = {submitHandler}>
-            <label for="date">Enter a date:</label>
-            <input type="date" id="date" name="date" required/>
+            <label for="date-input">Enter a date:</label>
+            <input type="date" id="date" name="date-input" required/>
             <label for="amtrec">Amount Recieved:</label>
             <input type="number" id="amtrec" name="amtrec" required/>
             <label for="moneyrf">Money Recieved From:</label>
-            <input type="text" id="moneyrf" name="moneyrf"  required />
+            <input type="text" id="moneyrecfrom" name="moneyrf"  required />
             <label for="reason">Reason:</label>
             <input type="text" id="reason" name="reason"  required />
             <label for="folionum">Folio Number:</label>
@@ -75,8 +75,6 @@ function Dayamtrecievedform() {
 
 
             <button type="submit" value='submit' >Submit</button>
-            <p style={{ color: 'brown' }}>{error}</p>
-
         </form>
           
           </div>
